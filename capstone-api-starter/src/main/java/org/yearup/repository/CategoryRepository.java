84a45625da741acd9d86d89286cb2a8e397a86c2
MichaelTS2001 <@Allansoccer_24>
2026-06-24 15:03:22 -0400

    Phase 1,2, ands 4 done

--- capstone-api-starter/src/main/java/org/yearup/repository/CategoryRepository.java
+++ capstone-api-starter/src/main/java/org/yearup/repository/CategoryRepository.java
@@ -7,12 +7,4 @@ import org.yearup.models.Category;
 @Repository
 public interface CategoryRepository extends JpaRepository<Category, Integer>
 {
-    //provide a CRUD
-    //Create a category of data category
-    //find by ID
-    //update a category
-    //find all
-    //delete by ID
-    //delete
-    //check existence
 }
